701f312e90397a0ee16041649433f45ba07032be
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-01-10 15:53:26 +0100

    add multiple pythons

--- modules/development/python/default.nix
+++ modules/development/python/default.nix
@@ -8,6 +8,8 @@
     # python software
     pipenv
     poetry
+    python37
+    python38
     (python39.withPackages (ps:
       with ps; [
         pip
@@ -18,5 +20,11 @@
         pyaml
         boto3
       ]))
+    python310
+    python311
   ];
+
+  environment.interactiveShellInit = ''
+    [[ -z "$HOME" ]] || export PATH="$HOME/.local/bin:$PATH"
+  '';
 }
