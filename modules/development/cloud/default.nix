5a5b8779e1f57d61099532f7e67732abeb16a664
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-02-09 15:02:49 +0100

    add `tf-fmt` script

--- modules/development/cloud/default.nix
+++ modules/development/cloud/default.nix
@@ -12,5 +12,28 @@
     # AWS
     awscli2
     eksctl
+
+    (pkgs.writeShellApplication {
+      name = "tf-fmt";
+      runtimeInputs = with pkgs; [ pkgs.gnugrep pkgs.gnused pkgs.coreutils pkgs.findutils ];
+      text = ''
+      since_revision="$1"
+      cd "$(git rev-parse --show-toplevel)"
+
+      if command -v terraform ; then
+        git diff --name-only "$since_revision" | grep -E '.(tf|tfvars)$' | sed 's#/[^/]*$##g' | sort | uniq \
+          | xargs -n1 -r terraform fmt
+      else
+        echo 'terraform executable is missing, skipping...'
+      fi
+
+      if command -v terragrunt ; then
+        git diff --name-only "$since_revision" | grep -E '.(hcl)$' \
+          | xargs -n1 -r terragrunt hclfmt --terragrunt-hclfmt-file
+      else
+        echo 'terragrunt executable is missing, skipping...'
+      fi
+      '';
+    })
   ];
 }
