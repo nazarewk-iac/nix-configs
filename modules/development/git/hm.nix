2237b1601b55e45bc08e74628c83126a345bd2c5
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-02-18 11:31:51 +0100

    improve GitHub scripts

--- modules/development/git/hm.nix
+++ modules/development/git/hm.nix
@@ -5,6 +5,47 @@ let
   relDir = "${cfg.baseDir}";
   absDir = "${config.home.homeDirectory}/${relDir}";
   shellDir = "$HOME/${relDir}";
+
+  ghDir = (pkgs.writeShellApplication {
+    name = "gh-dir";
+    runtimeInputs = with pkgs; [  ];
+    text = ''
+      for entry in "$@"; do
+        echo "${shellDir}/$entry"
+      done
+    '';
+  });
+
+  ghRemote = (pkgs.writeShellApplication {
+    name = "gh-remote";
+    runtimeInputs = with pkgs; [  ];
+    text = ''
+      for entry in "$@"; do
+        org="''${entry%/*}"
+        repo="''${entry#*/}"
+        echo "${cfg.remoteShellPattern}"
+      done
+    '';
+  });
+
+  ghClone = (pkgs.writeShellApplication {
+    name = "gh-clone";
+    runtimeInputs = with pkgs; [ git ghDir ghRemote ];
+    text = ''
+      for entry in "$@"; do
+        dir="$(gh-dir "$entry")"
+        remote="$(gh-remote "$entry")"
+
+        if [ -d "$dir/.git" ] ; then
+          echo "$dir already exists, skipping..."
+          continue
+        fi
+
+        git clone "$remote" "$dir"
+      done
+    '';
+  });
+
 in {
   options.nazarewk.development.git = {
     enable = mkEnableOption "Git development utilities";
@@ -19,18 +60,17 @@ in {
     };
   };
   config = mkIf cfg.enable {
+    programs.bash.initExtra = config.programs.zsh.initExtra;
+    programs.zsh.initExtra = ''
+      gh-cd() {
+        cd "$(${ghDir}/bin/gh-dir $1)"
+      }
+    '';
+
     home.packages = with pkgs; [
-      (pkgs.writeShellApplication {
-        name = "gh-clone";
-        runtimeInputs = with pkgs; [ git ];
-        text = ''
-          for entry in "$@"; do
-            org="''${entry%/*}"
-            repo="''${entry#*/}"
-            git clone "${cfg.remoteShellPattern}" "${shellDir}/$entry"
-          done
-        '';
-      })
+      ghDir
+      ghRemote
+      ghClone
     ];
   };
 }
