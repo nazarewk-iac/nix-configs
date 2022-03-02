7fa4bc54a78af1a5969ea4156342f6d58ef70a87
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-03-02 16:17:31 +0100

    override `wlroots`
    see:
    - https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/3469/commits
    - https://github.com/swaywm/sway/issues/6856

--- modules/desktop/base/default.nix
+++ modules/desktop/base/default.nix
@@ -10,6 +10,20 @@ in {
   config = mkIf cfg.enable {
     nixpkgs.overlays = [
       flakeInputs.nixpkgs-wayland.overlay
+
+      (self: super: {
+        wlroots = super.wlroots.overrideAttrs (old: {
+          src = super.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "wlroots";
+            repo = "wlroots";
+            # report: https://github.com/swaywm/sway/issues/6856
+            # overriden version: https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/3469/commits
+            rev = "15008750db82115fbdcf77d1fccbde9a91d70a06";
+            sha256 = "sha256-T3LhUaczQVp0mj/vKpxRb7PLwZk0iEsejOqSWtQMw8k=";
+          };
+        });
+      })
     ];
 
     fonts.fonts = with pkgs; [
