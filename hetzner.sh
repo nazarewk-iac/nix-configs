06215bdf8b8c665f36414da2838fa9903fd97284
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-05-11 13:04:20 +0200

    fix restricted mode errors
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- hetzner.sh
+++ hetzner.sh
@@ -39,10 +39,15 @@ cmd mkfs.ext4 /dev/sda1
 cmd mount /dev/sda1 /mnt
 
 checkout_dir=/mnt/home/nazarewk/dev/github.com/nazarewk-iac
+repo_dir="${checkout_dir}/nix-configs"
 cmd mkdir -p "${checkout_dir}"
-cmd git clone https://github.com/nazarewk-iac/nix-configs.git "${checkout_dir}/nix-configs"
+if [ -d "${repo_dir}" ] ; then
+  cmd git pull "${repo_dir}"
+else
+  cmd git clone https://github.com/nazarewk-iac/nix-configs.git "${repo_dir}"
+fi
 cmd chown -R 1000:100 /mnt/home/nazarewk
 
 cmd mkdir -p /mnt/etc/nixos
-cmd ln -s "${checkout_dir/"/mnt"/"../.."}/nix-configs/flake.nix" /mnt/etc/nixos/flake.nix
-cmd nixos-install --show-trace --root /mnt --flake "${checkout_dir}/nix-configs#${HOST}"
+cmd ln -s "${repo_dir/"/mnt"/"../.."}/flake.nix" /mnt/etc/nixos/flake.nix
+cmd nixos-install --impure --show-trace --root /mnt --flake "${repo_dir}#${HOST}"
