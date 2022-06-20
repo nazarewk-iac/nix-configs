e81bf4f5526e8103e7a8076d3a40a4418555d156
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-06-20 14:57:13 +0200

    wireguard: move allowedIPsAsRoutes to proper place
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/networking/wireguard/default.nix
+++ modules/networking/wireguard/default.nix
@@ -24,7 +24,6 @@ let
     (builtins.mapAttrs (name: entry: mkMerge [
       (mkIf (!entry.server.enable) {
         allowedIPs = [ "${getIP entry.hostnum}/32" ];
-        allowedIPsAsRoutes = false;
       })
       (mkIf entry.server.enable {
         allowedIPs = [
@@ -138,6 +137,7 @@ in
 
       networking.wireguard.interfaces = {
         ${cfg.interfaceName} = {
+          allowedIPsAsRoutes = false;
           # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
           # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
           postSetup = ''
