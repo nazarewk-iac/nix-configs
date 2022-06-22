c58d23104efbb9d572cbd228b820c242e74f67e8
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-06-22 09:38:14 +0200

    nazarewk.virtualization.nixops.libvirtd: add missing qemu
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/virtualization/nixops/libvirtd/default.nix
+++ modules/virtualization/nixops/libvirtd/default.nix
@@ -14,6 +14,7 @@ in
 
     environment.systemPackages = with pkgs; [
       virt-manager
+      config.virtualisation.libvirtd.qemu.package
     ];
 
     system.activationScripts.nazarewkLibVirtdInit =
