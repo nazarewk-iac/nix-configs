559407e3823c61c2d1f635d52c7c35fbcc4057ef
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-07-14 11:33:42 +0200

    configure Prometheus with Grafana
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/monitoring/prometheus-stack/default.nix
+++ modules/monitoring/prometheus-stack/default.nix
@@ -6,29 +6,72 @@ in
 {
   options.nazarewk.monitoring.prometheus-stack = {
     enable = mkEnableOption "prometheus + grafana";
-  };
 
-  config = {
-    services.grafana = {
-      enable = true;
-      domain = "grafana.localhost";
-      port = 2342;
-      addr = "127.0.0.1";
+    pushgateway = {
+      enable = mkEnableOption "prometheus + grafana";
     };
 
-    services.prometheus = {
-      enable = true;
-      port = 9090;
-      listenAddress = "127.0.0.1";
-      retentionTime = "30d";
-      extraFlags = [
-        "--storage.tsdb.retention.size=1GB"
+    retentionSize = mkOption {
+      type = types.str;
+      default = "5GB";
+    };
+
+    retentionTime = mkOption {
+      type = types.str;
+      default = "90d";
+    };
+
+    listenAddress = mkOption {
+      type = types.str;
+      default = "127.0.0.1";
+    };
+  };
+
+  config = mkMerge [
+    ({
+      services.grafana = {
+        enable = true;
+        domain = "grafana.localhost";
+        port = 2342;
+        addr = cfg.listenAddress;
+
+        provision.enable = true;
+        provision.datasources = [
+          {
+            url = "http://${cfg.listenAddress}:9090";
+            type = "prometheus";
+            name = "Prometheus";
+          }
+        ];
+      };
+
+      services.grafana-image-renderer = {
+        enable = true;
+        provisionGrafana = true;
+        settings.rendering.height = 1000;
+        settings.rendering.width = 1900;
+      };
+
+      services.prometheus = {
+        enable = true;
+        port = 9090;
+        listenAddress = cfg.listenAddress;
+        retentionTime = cfg.retentionTime;
+        extraFlags = [
+          "--storage.tsdb.retention.size=${cfg.retentionSize}"
+        ];
+      };
+
+      environment.systemPackages = with pkgs; [
+        prometheus
+        opentsdb
       ];
-    };
-
-    environment.systemPackages = with pkgs; [
-      prometheus
-      opentsdb
-    ];
-  };
+    })
+    (mkIf cfg.pushgateway.enable {
+      services.prometheus.pushgateway = {
+        enable = true;
+        web.listen-address = "${cfg.listenAddress}:9091";
+      };
+    })
+  ];
 }
