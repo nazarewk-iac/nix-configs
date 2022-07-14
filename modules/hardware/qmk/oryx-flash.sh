621a1c4536e91dbcbcd30de8dddac6d363d790ae
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-07-14 15:41:42 +0200

    fix
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/hardware/qmk/oryx-flash.sh
+++ modules/hardware/qmk/oryx-flash.sh
@@ -9,7 +9,7 @@ else
   revision=latest
 fi
 
-file="$(mktemp -t /tmp/oryx-flash.XXXX.bin)"
+file="$(mktemp -t oryx-flash.XXXX.bin)"
 trap 'rm $file || :' EXIT
 curl -L "https://oryx.zsa.io/${layout_id}/${revision}/binary" -o "${file}"
 wally-cli "${file}"
--- modules/hardware/qmk/oryx-src.sh
+++ modules/hardware/qmk/oryx-src.sh
@@ -11,7 +11,7 @@ fi
 
 output_dir="${2:-"."}"
 
-file="$(mktemp -t /tmp/oryx-src.XXXX.zip)"
+file="$(mktemp -t oryx-src.XXXX.zip)"
 trap 'rm $file || :' EXIT
 curl -L "https://oryx.zsa.io/${layout_id}/${revision}/source" -o "${file}"
 unzip -d "${output_dir}" "${file}"
