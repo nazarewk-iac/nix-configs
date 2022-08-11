60317b6868917983c8625ef754069a3b47eceb12
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-08-11 09:46:47 +0200

    add csv2json & jiq
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/development/data/default.nix
+++ modules/development/data/default.nix
@@ -3,13 +3,15 @@ with lib;
 let
   cfg = config.nazarewk.development.data;
 
-  yq = cfg.packages.yq;
-  yj = cfg.packages.yj;
+  yq = "${cfg.packages.yq}/bin/yq";
+  yj = "${cfg.packages.yj}/bin/yj";
+  miller = "${cfg.packages.miller}/bin/mlr";
 
-  converter = name: cmd: flags: pkgs.writeShellApplication {
+  converter = name: opts: pkgs.writeShellApplication {
     name = name;
+    runtimeInputs = opts.pkgs;
     text = ''
-      args=(${flags})
+      args=(${escapeShellArgs opts.args})
       files=()
       for arg in "$@" ; do
         if [[ "$arg" = -* ]]; then
@@ -19,17 +21,39 @@ let
         fi
       done
       case "''${#files[@]}" in
-        0) "${cmd}" "''${args[@]}" ;;
-        1) "${cmd}" "''${args[@]}" < "''${files[0]}" ;;
-        2) "${cmd}" "''${args[@]}" < "''${files[0]}" > "''${files[1]}" ;;
+        0) "''${args[@]}" ;;
+        1) "''${args[@]}" < "''${files[0]}" ;;
+        2) "''${args[@]}" < "''${files[0]}" > "''${files[1]}" ;;
         *)
           echo 'only 2 files (input and output) can be passed!'
           exit 1
         ;;
       esac
-
     '';
   };
+
+  converterPkgs = mapAttrs (name: opts: converter name opts) {
+    "csv2json" = { pkgs = [ miller ]; args = [ "mlr" "--icsv" "--ojson" "cat" ]; };
+    "csv2jsonl" = { pkgs = [ miller ]; args = [ "mlr" "--icsv" "--ojsonl" "cat" ]; };
+    "hcl2hcl" = { pkgs = [ yj ]; args = [ "yj" "-cc" ]; };
+    "hcl2json" = { pkgs = [ yj ]; args = [ "yj" "-cj" ]; };
+    "hcl2toml" = { pkgs = [ yj ]; args = [ "yj" "-ct" ]; };
+    "hcl2yaml" = { pkgs = [ yj ]; args = [ "yj" "-cy" ]; };
+    "json2hcl" = { pkgs = [ yj ]; args = [ "yj" "-jc" ]; };
+    "json2json" = { pkgs = [ yj ]; args = [ "yj" "-jj" ]; };
+    "json2toml" = { pkgs = [ yj ]; args = [ "yj" "-jt" ]; };
+    "json2yaml" = { pkgs = [ yj ]; args = [ "yj" "-jy" ]; };
+    "toml2hcl" = { pkgs = [ yj ]; args = [ "yj" "-tc" ]; };
+    "toml2json" = { pkgs = [ yj ]; args = [ "yj" "-tj" ]; };
+    "toml2toml" = { pkgs = [ yj ]; args = [ "yj" "-tt" ]; };
+    "toml2yaml" = { pkgs = [ yj ]; args = [ "yj" "-ty" ]; };
+    "yaml2hcl" = { pkgs = [ yj ]; args = [ "yj" "-yc" ]; };
+    "yaml2json" = { pkgs = [ yq ]; args = [ "yq" "-M --" ]; };
+    "yaml2toml" = { pkgs = [ yj ]; args = [ "yj" "-yt" ]; };
+    "yaml2yaml" = { pkgs = [ yj ]; args = [ "yj" "-yy" ]; };
+    "yamls2json" = { pkgs = [ yq ]; args = [ "yq" "-M '[inputs]' --" ]; };
+  };
+  conv = mapAttrs (name: pkg: "${pkg}/bin/${name}") converterPkgs;
 in
 {
   options.nazarewk.development.data = {
@@ -45,35 +69,30 @@ in
         type = types.package;
         default = pkgs.yj;
       };
+
+      miller = mkOption {
+        type = types.package;
+        default = pkgs.miller;
+      };
+
+      jq = mkOption {
+        type = types.package;
+        default = pkgs.jq;
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      yq
-      yj
-      jq
+    environment.systemPackages = (with pkgs; [
+      cfg.packages.miller
+      cfg.packages.yq
+      cfg.packages.jq
+      cfg.packages.yj
+
+      jiq
       cue
 
       gnused
-
-      (converter "hcl2hcl" "${yj}/bin/yj" "-cc")
-      (converter "hcl2json" "${yj}/bin/yj" "-cj")
-      (converter "hcl2toml" "${yj}/bin/yj" "-ct")
-      (converter "hcl2yaml" "${yj}/bin/yj" "-cy")
-      (converter "json2hcl" "${yj}/bin/yj" "-jc")
-      (converter "json2json" "${yj}/bin/yj" "-jj")
-      (converter "json2toml" "${yj}/bin/yj" "-jt")
-      (converter "json2yaml" "${yj}/bin/yj" "-jy")
-      (converter "toml2hcl" "${yj}/bin/yj" "-tc")
-      (converter "toml2json" "${yj}/bin/yj" "-tj")
-      (converter "toml2toml" "${yj}/bin/yj" "-tt")
-      (converter "toml2yaml" "${yj}/bin/yj" "-ty")
-      (converter "yaml2hcl" "${yj}/bin/yj" "-yc")
-      (converter "yaml2json" "${yq}/bin/yq" "-M --")
-      (converter "yaml2toml" "${yj}/bin/yj" "-yt")
-      (converter "yaml2yaml" "${yj}/bin/yj" "-yy")
-      (converter "yamls2json" "${yq}/bin/yq" "-M '[inputs]' --")
-    ];
+    ]) ++ (attrValues converterPkgs);
   };
 }
