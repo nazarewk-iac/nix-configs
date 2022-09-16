8507464eab3575f423966133944c7097d15ab224
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-09-16 14:55:09 +0200

    polkit log all

--- modules/programs/gnupg/pcsc-lite-rules.js
+++ modules/programs/gnupg/pcsc-lite-rules.js
@@ -1,4 +1,7 @@
 polkit.addRule(function (action, subject) {
+    polkit.log("action=" + action);
+    polkit.log("subject=" + subject);
+
     if (subject.isInGroup("wheel") && (
         action.id === "org.debian.pcsc-lite.access_pcsc" ||
         action.id === "org.debian.pcsc-lite.access_card"
