63bcdf213f83f901b96bae113b498f665f47b479
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-09-16 10:11:44 +0200

    gpg pinned as an overlay

--- modules/hardware/yubikey/default.nix
+++ modules/hardware/yubikey/default.nix
@@ -61,11 +61,16 @@ in
       yubico-pam
     ];
 
-    # see https://dev.gnupg.org/T6179
-    # stick to 2.3.6 due to errors in 2.3.7:
-    # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: detected reader 'Yubico YubiKey OTP+FIDO+CCID 00 00'
-    # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: DBG: Curve with OID not supported:  2b06010401da470f01
-    # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: no supported card application found: Card error
-    programs.gnupg.package = flakeInputs.nixpkgs-gpg236.legacyPackages.${system}.gnupg;
+    nixpkgs.overlays = [
+      (self: super:
+        {
+          # see https://dev.gnupg.org/T6179
+          # stick to 2.3.6 due to errors in 2.3.7:
+          # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: detected reader 'Yubico YubiKey OTP+FIDO+CCID 00 00'
+          # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: DBG: Curve with OID not supported:  2b06010401da470f01
+          # Aug 31 11:45:58 gpg-agent[40089]: scdaemon[40089]: no supported card application found: Card error
+          gnupg = flakeInputs.nixpkgs-gpg236.legacyPackages.${system}.gnupg;
+        })
+    ];
   };
 }
