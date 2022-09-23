745e4fe6e8ba22e1f40a2afd7167db9c8f014c3e
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-09-23 13:01:55 +0200

    add Lua libs
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/development/lua/default.nix
+++ modules/development/lua/default.nix
@@ -3,32 +3,39 @@ with lib;
 let
   cfg = config.nazarewk.development.lua;
 
-  mkAltLua = major: minor:
-    let
-      M = toString major;
-      m = toString minor;
-      suffixedBinaries = pkg: suffix: pkgs.runCommand "${pkg.name}-suffixed-bin-${suffix}" { } ''
-        mkdir -p $out/bin
-        for src in ${pkg}/bin/* ; do
-          dst="''${src##*/}${suffix}"
-          ln -s "$src" "$out/bin/$dst"
-        done
-      '';
-    in
-    suffixedBinaries pkgs."lua${M}_${m}" "${M}.${m}";
+  getLua = pkg: (pkg.withPackages (ps: map (n: ps.${n}) cfg.extraPackages));
+
+
+  suffixedBinaries = pkg: suffix: pkgs.runCommand "${pkg.name}-suffixed-bin-${suffix}" { } ''
+    mkdir -p $out/bin
+    for src in ${pkg}/bin/* ; do
+      dst="''${src##*/}${suffix}"
+      ln -s "$src" "$out/bin/$dst"
+    done
+  '';
 in
 {
   options.nazarewk.development.lua = {
     enable = mkEnableOption "lua development";
+
+    extraPackages = mkOption {
+      type = types.listOf types.str;
+      default = [
+        "lyaml"
+        "stdlib"
+        "luarepl"
+        "luarocks"
+      ];
+    };
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      lua5_4 # latest
-      (mkAltLua 5 1) # argocd
-      (mkAltLua 5 2)
-      (mkAltLua 5 3)
-      (mkAltLua 5 4)
+      (getLua lua5_4) # latest
+      (suffixedBinaries (getLua lua5_1) "5.1") # argocd
+      (suffixedBinaries (getLua lua5_2) "5.2")
+      (suffixedBinaries (getLua lua5_3) "5.3")
+      (suffixedBinaries (getLua lua5_4) "5.4")
     ];
   };
 }
