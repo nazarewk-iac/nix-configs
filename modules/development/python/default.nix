c02849d8188e1cff93dae0020debf16945ffca4f
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-11-04 11:10:09 +0100

    make multiple pythons
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/development/python/default.nix
+++ modules/development/python/default.nix
@@ -2,6 +2,50 @@
 with lib;
 let
   cfg = config.kdn.development.python;
+
+  mkPython = pkg: (pkg.withPackages (ps: with ps; [
+    black
+    boto3
+    build # build a package, see https://realpython.com/pypi-publish-python-package/#build-your-package
+    cookiecutter
+    diagrams
+    flake8
+    graphviz
+    ipython
+    isort
+    matplotlib
+    pip
+    # pip-tools  # 2022-08-31 fails on `assert out.exit_code == 2` @ test_bad_setup_file()
+    pipx
+    pyaml
+    pyheos
+    pytest
+    pyyaml
+    requests
+    ruamel-yaml
+    twine # upload to pypi, see https://realpython.com/pypi-publish-python-package/#upload-your-package
+  ]));
+
+  renamedBinaries = fmt: pkg: pkgs.runCommand "${pkg.name}-renamed-to-${builtins.replaceStrings [ "%s" ] [ "BIN" ] fmt}"
+    { buildInputs = [ ]; } ''
+    set -x
+    ${lib.toShellVar "srcDir" "${pkg}/bin"}
+    ${lib.toShellVar "fmt" fmt}
+
+    mkdir -p "$out/bin"
+    if test "$fmt" = "%s" ; then
+      echo "fmt $fmt must modify filename!"
+      exit 1
+    fi
+
+    for file in "$srcDir"/* ; do
+      test -x "$file" || continue
+      filename="''${file##*/}"
+      renamed="$(printf "$fmt" "$filename")"
+      ln -sfT "$file" "$out/bin/$renamed"
+    done
+    set +x
+  '';
 in
 {
   options.kdn.development.python = {
@@ -26,6 +70,11 @@ in
       # python software
       pipenv
       poetry
+      (renamedBinaries "%s3.8" python38)
+      (renamedBinaries "%s3.9" python39)
+      (mkPython python310)
+      (renamedBinaries "%s3.10" (mkPython python310))
+      (renamedBinaries "%s3.11" python311)
       (python310.withPackages (ps: with ps; [
         black
         boto3
