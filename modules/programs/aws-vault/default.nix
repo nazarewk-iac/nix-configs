f55927a519ec37cbb6bbc525ce4bdfec4904052f
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-11-15 12:37:38 +0100

    drop AWS_ASSUME_ROLE_TTL
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- modules/programs/aws-vault/default.nix
+++ modules/programs/aws-vault/default.nix
@@ -44,7 +44,6 @@ in
     defaultEnv = mkOption {
       default = {
         AWS_VAULT_PROMPT = "ykman";
-        AWS_ASSUME_ROLE_TTL = "8h";
         AWS_VAULT_BACKEND = "pass";
         AWS_VAULT_PASS_PREFIX = "aws-vault";
       };
