1a14d70dd5d080990505ffd7531f7c5577e42687
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-11-24 07:31:27 +0100

    keyring-pass: move to poetry2nix
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- packages/git-credential-keyring/default.nix
+++ packages/git-credential-keyring/default.nix
@@ -1,13 +1,4 @@
 { pkgs, ... }:
 pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./.;
-
-  # see https://github.com/nix-community/poetry2nix/blob/5aa37b8a5652a4c2a2372e82815ebd15db07f087/docs/edgecases.md?plain=1#L137-L149
-  overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (final: prev: {
-    keyring-pass = prev.keyring-pass.overridePythonAttrs (
-      old: {
-        buildInputs = (old.buildInputs or [ ]) ++ [ final.poetry ];
-      }
-    );
-  });
 }
