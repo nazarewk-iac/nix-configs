76ddc6370b7309a5cd5abff155e20e91c078e660
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-12-12 11:49:37 +0100

    rambox: sync with PR
    Signed-off-by: Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>

--- packages/rambox/default.nix
+++ packages/rambox/default.nix
@@ -3,7 +3,6 @@
 let
   pname = "rambox";
   version = "2.0.9";
-  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/ramboxapp/download/releases/download/v${version}/Rambox-${version}-linux-x64.AppImage";
@@ -13,29 +12,27 @@ let
   desktopItem = (makeDesktopItem {
     desktopName = "Rambox";
     name = pname;
-    exec = pname;
+    exec = "rambox";
     icon = pname;
     categories = [ "Network" ];
   });
 
   appimageContents = appimageTools.extractType2 {
-    inherit name src;
+    inherit pname version src;
   };
 in
 appimageTools.wrapType2 {
-  inherit name src;
+  inherit pname version src;
 
   extraInstallCommands = ''
     mkdir -p $out/share/applications $out/share/icons/hicolor/256x256/apps
-    # CE uses rambox-<version>, Pro uses rambox
-    mv $out/bin/rambox* $out/bin/${pname}
     install -Dm644 ${appimageContents}/usr/share/icons/hicolor/256x256/apps/rambox*.png $out/share/icons/hicolor/256x256/apps/${pname}.png
     install -Dm644 ${desktopItem}/share/applications/* $out/share/applications
   '';
 
   meta = with lib; {
-    description = "Free and Open Source messaging and emailing app that combines common web applications into one";
-    homepage = "https://rambox.pro";
+    description = "Workspace Simplifier - a cross-platform application organizing web services into Workspaces similar to browser profiles.";
+    homepage = "https://rambox.app";
     license = licenses.unfree;
     maintainers = with maintainers; [ nazarewk ];
     platforms = [ "x86_64-linux" ];
