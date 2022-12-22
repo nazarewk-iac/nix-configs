7ef71d65a909ff31dfbdd292486022119a70ebdf
Krzysztof Nazarewski <gpg@kdn.im>
2022-12-22 10:15:42 +0100

    WIP: oams setup
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/profile/host/oams/default.nix
+++ modules/profile/host/oams/default.nix
@@ -1,6 +1,60 @@
 { config, pkgs, lib, ... }:
 let
   cfg = config.kdn.profile.host.oams;
+
+  headerFilename = "main-header.img";
+  # see https://wiki.archlinux.org/title/Dm-crypt/Specialties#Using_systemd_hook
+  # Replace XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX with the LUKS super block UUID. It can be acquired with `cryptsetup luksDump header.img` or `sudo blkid -s UUID -o value header.img`
+  rootUUID = "c4b9bdbc-900f-482e-8fa6-6c6824c560e9";
+  luksDevice = "/dev/disk/by-id/ata-Samsung_Portable_SSD_T5_S49TNP0KC01288A";
+
+  trim = strip: txt: lib.pipe txt [
+    (lib.strings.removePrefix strip)
+    (lib.strings.removeSuffix strip)
+  ];
+
+  mountScriptLines = lib.pipe (builtins.readFile ./mount.sh) [
+    (lib.strings.splitString "\n")
+  ];
+
+  zfsPrefix = lib.pipe mountScriptLines [
+    (lib.filter (lib.hasPrefix "zfs_prefix="))
+    builtins.head
+    (lib.strings.splitString "=")
+    lib.lists.last
+    (trim ''"'')
+  ];
+
+  zpool = lib.pipe zfsPrefix [
+    (lib.strings.splitString "/")
+    builtins.head
+  ];
+
+  bootPath = lib.pipe mountScriptLines [
+    (lib.filter (lib.hasInfix "/dev/disk/by-uuid"))
+    builtins.head
+    (lib.strings.splitString " ")
+    (lib.filter (lib.hasInfix "/dev/disk/by-uuid"))
+    builtins.head
+    (trim ''"'')
+  ];
+
+  bootUUID = lib.pipe bootPath [
+    (lib.strings.splitString "/")
+    lib.lists.last
+  ];
+
+  zfsMountPaths = lib.pipe mountScriptLines [
+    (lib.filter (lib.hasPrefix "setupZFS "))
+    (builtins.map (entry: lib.pipe entry [
+      # for whatever reason (lib.strings.splitString " ") was not working, using builtins.split then filter,
+      # becaus split is regex-based and returns match lists in between
+      (builtins.split " +")
+      (builtins.filter builtins.isString)
+      (l: builtins.elemAt l 1)
+      (trim ''"'')
+    ]))
+  ];
 in
 {
   options.kdn.profile.host.oams = {
@@ -10,5 +64,33 @@ in
   config = lib.mkIf cfg.enable {
     kdn.profile.machine.workstation.enable = true;
     kdn.hardware.gpu.amd.enable = true;
+
+    boot.zfs.forceImportRoot = false;
+
+    boot.kernelParams = [
+      "rd.luks.name=${rootUUID}=${zpool}"
+      "rd.luks.options=${rootUUID}=header=/${headerFilename}:UUID=${bootUUID}"
+      "rd.luks.data=${rootUUID}=${luksDevice}"
+    ];
+
+    fileSystems = lib.pipe [
+      {
+        "/boot" = {
+          device = bootPath;
+          fsType = "vfat";
+        };
+      }
+      (lib.pipe zfsMountPaths [
+        (builtins.map (path: {
+          "${path}" = {
+            device = "${zfsPrefix}/${path}";
+            fsType = "zfs";
+          };
+        }))
+      ])
+    ] [
+      lib.lists.flatten
+      lib.mkMerge
+    ];
   };
 }
--- modules/profile/host/oams/mount.sh
+++ modules/profile/host/oams/mount.sh
@@ -2,54 +2,84 @@
 set -xeEuo pipefail
 
 target="${1:-"/mnt"}"
-zpool="oams-primary"
-zfs_prefix="${zpool}/oams"
+zfs_prefix="oams-main/fs"
+zpool="${zfs_prefix%%/*}"
+
 if [ "${APPLY:-}" = 1 ]; then
-  cmd() { "$@"; }
+  function cmd() { "$@"; }
 else
-  cmd() { echo "$@"; }
+  function cmd() { echo "$@"; }
   test -n "${DEBUG:-}" || set +x
 fi
 
-mnt() {
-  local mountpoint="${!#}"
-  if mountpoint "${mountpoint}"; then
+function mnt() {
+  local current_mountpoint="${!#}"
+  if mountpoint "${current_mountpoint}"; then
     return 0
   fi
-  cmd mkdir -p "${mountpoint}"
+  cmd mkdir -p "${current_mountpoint}"
   cmd mount "$@"
 }
 
-mntZFS() {
+function ensureZFS() {
   local dataset_name="${1}"
-  local prefix="${2:-}"
-  local path="${3:-"${dataset_name}"}"
-  path="${path%/}"
-  local mountpoint="${target%/}/${path#/}"
-  local dataset="${zfs_prefix}${prefix}${dataset_name}"
-  mnt -t zfs "${dataset}" "${mountpoint}"
+  local dataset="${zfs_prefix}${dataset_name%/}"
+  if ! zfs list "${dataset}" &>/dev/null; then
+    cmd zfs create "${dataset}"
+  fi
 }
 
-zpool status "${zpool}" || cmd zpool import "${zpool}"
+function setupZFS() {
+  local dataset_name="${1}"
+  ensureZFS "${dataset_name}"
+  local mountpoint="${dataset_name%/}"
+  [[ -n "${mountpoint}" ]] || mountpoint=/
+  local current_mountpoint="${target%/}/${mountpoint#/}"
+  current_mountpoint="${current_mountpoint%/}"
+  local dataset="${zfs_prefix}${dataset_name%/}"
+  local old_mountpoint
+  old_mountpoint="$(zfs list -o mountpoint "${dataset}" | tail -n 1)"
+  [ "${old_mountpoint}" != "none" ] || old_mountpoint=
+  if [ "${current_mountpoint}" != "${old_mountpoint}" ]; then
+    cmd zfs set "mountpoint=${mountpoint}" "${dataset}"
+  fi
+  if mountpoint "${current_mountpoint}"; then
+    return 0
+  fi
+  cmd mkdir -p "${current_mountpoint}"
+  cmd zfs mount "${dataset}"
+}
+
+altroot="$(zpool list -o altroot "${zpool}" | tail -n 1)"
+if [ -n "${altroot}" ] && [ "${altroot}" != "${target}" ]; then
+  echo "altroot=${altroot} does not equal target=${target}!"
+  exit 1
+fi
+
+zpool status "${zpool}" || cmd zpool import -N -R "${target}" "${zpool}"
 cmd zfs load-key -a
 
-mntZFS "/root" "/nixos" "/"
-mnt -t vfat "/dev/disk/by-uuid/2BFB-6A81" "${target}/boot"
-mntZFS "/etc" "/nixos"
-mntZFS "/nix" "/nixos"
-mntZFS "/var" "/nixos"
-mntZFS "/var/lib/libvirt" "/nixos"
-mntZFS "/var/lib/rook" "/nixos"
-mntZFS "/var/lib/microvms" "/nixos"
-mntZFS "/var/log" "/nixos"
-mntZFS "/var/log/journal" "/nixos"
-mntZFS "/var/spool" "/nixos"
-
-mntZFS "/home"
-mntZFS "/home/kdn"
-mntZFS "/home/kdn/.cache"
-mntZFS "/home/kdn/Downloads"
-mntZFS "/home/kdn/Nextcloud"
-mntZFS "/home/kdn/.local"
-mntZFS "/home/kdn/.local/share"
-mntZFS "/home/kdn/.local/share/containers"
+ensureZFS ""
+setupZFS "/"
+mnt -t vfat "/dev/disk/by-uuid/1290-B5A3" "${target}/boot"
+setupZFS "/etc"
+setupZFS "/home"
+setupZFS "/home/kdn"
+setupZFS "/home/kdn/.cache"
+setupZFS "/home/kdn/.config"
+setupZFS "/home/kdn/.local"
+setupZFS "/home/kdn/.local/share"
+setupZFS "/home/kdn/.local/share/containers"
+setupZFS "/home/kdn/Downloads"
+setupZFS "/home/kdn/Nextcloud"
+setupZFS "/home/kdn/dev"
+setupZFS "/nix"
+setupZFS "/nix/store"
+setupZFS "/nix/var"
+setupZFS "/var"
+setupZFS "/var/lib"
+setupZFS "/var/lib/libvirt"
+setupZFS "/var/lib/microvms"
+setupZFS "/var/log"
+setupZFS "/var/log/journal"
+setupZFS "/var/spool"
