8f961523932628ac1de3b916f6c2cf7d3f954e4a
Krzysztof Nazarewski <gpg@kdn.im>
2023-11-02 12:13:00 +0100

    packages: undo poetry2nix follows
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- packages/flake.nix
+++ packages/flake.nix
@@ -2,18 +2,9 @@
   description = "Description for the project";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
-    nix-github-actions.url = "github:nix-community/nix-github-actions";
-    poetry2nix.inputs.flake-utils.follows = "flake-utils";
-    poetry2nix.inputs.nix-github-actions.follows = "nix-github-actions";
     poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
-    poetry2nix.inputs.treefmt-nix.follows = "treefmt-nix";
     poetry2nix.url = "github:nix-community/poetry2nix";
     systems.url = "github:nix-systems/default";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs@{ flake-parts, self, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
