8db3f6e9d06d9c048d629146c6f2267d7db66251
Krzysztof Nazarewski <gpg@kdn.im>
2023-12-20 16:44:32 +0100

    dunst: configure timeouts
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/desktop/sway/home-manager/notifications.nix
+++ modules/desktop/sway/home-manager/notifications.nix
@@ -5,5 +5,12 @@ in
 {
   config = lib.mkIf (config.kdn.headless.enableGUI && cfg.enable) {
     services.dunst.enable = true;
+    services.dunst.settings = {
+      global = {
+        idle_threshold = "15s";
+        ignore_dbusclose = true;
+        timeout = "30m";
+      };
+    };
   };
 }
