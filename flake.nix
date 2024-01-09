b9d935fca851e63a719e9bf6242c91bb10e1fe7d
Krzysztof Nazarewski <gpg@kdn.im>
2024-01-09 14:15:58 +0100

    hardware.edid: pin to merged branch
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nazarewk/nixpkgs/hardware-firmware-edid-ergonomy";
+    nixpkgs.url = "github:nazarewk/nixpkgs/edids";
     nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
 
     base16.url = "github:SenchoPens/base16.nix";
