4dec04dc1fd3bc3a3cd76026a145cd2d3068eaa8
Krzysztof Nazarewski <gpg@kdn.im>
2024-01-19 12:53:08 +0100

    nixos/netclient: add NETCLIENT_INIT_TYPE & NETCLIENT_AUTO_UPDATE
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/networking/netmaker/client/default.nix
+++ modules/networking/netmaker/client/default.nix
@@ -48,11 +48,14 @@ in
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
       services.netclient.enable = true;
-      services.netclient.package = pkgs.kdn.netclient;
+      services.netclient.package = pkgs.writeShellScriptBin cfg.package.name ''
+        export NETCLIENT_INIT_TYPE="''${NETCLIENT_INIT_TYPE:-"systemd"}"
+        export NETCLIENT_AUTO_UPDATE="''${NETCLIENT_AUTO_UPDATE:-"disabled"}"
+        exec ${lib.getExe cfg.package} "$@"
+      '';
 
       # required for detection by https://github.com/gravitl/netclient/blob/51f4458db0a5560d102d337a342567cb347399a6/config/config.go#L430-L443
       systemd.services."netclient".path = let fw = config.networking.firewall; in lib.optionals fw.enable [ fw.package ];
-      systemd.services.netclient.environment.NETCLIENT_INIT_TYPE = "systemd";
       networking.networkmanager.unmanaged = [ "interface-name:netmaker*" ];
 
       networking.firewall = with cfg.firewall.ports; {
