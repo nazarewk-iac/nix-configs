3603f8cd8f09ded53ca9a67b28de2313f1e66e5f
Krzysztof Nazarewski <gpg@kdn.im>
2024-01-31 15:22:59 +0100

    obler: switch theme to light
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/profile/host/obler/default.nix
+++ modules/profile/host/obler/default.nix
@@ -54,5 +54,7 @@ in
       url = "https://nc.nazarewk.pw/s/q63pjY9H93faf5t/download/lake-view-with-light-blue-water-a6cnqa1pki4g69jt.jpg";
       sha256 = "sha256-0Dyc9Kj9IkStIJDXw9zlEFHqc2Q5WruPSk/KapM7KgM=";
     };
+    stylix.polarity = "light";
+    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/standardized-light.yaml";
   };
 }
