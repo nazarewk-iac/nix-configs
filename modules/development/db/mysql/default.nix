dce16f738c42285b1d65def617fc45a1b6d7e288
Krzysztof Nazarewski <gpg@kdn.im>
2024-03-15 07:27:33 +0100

    feat: use `usql` for database client
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/development/db/mysql/default.nix
+++ modules/development/db/mysql/default.nix
@@ -4,16 +4,12 @@ let
 in
 {
   options.kdn.development.db.mysql = {
-    enable = lib.mkEnableOption "MySQL development";
+    enable = lib.mkEnableOption "SQL development and access";
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      mariadb
-      # mysql80 # TODO: doesn't build https://github.com/NixOS/nixpkgs/issues/226673
-      # TODO: requires v8_8_x, but it's currently broken https://github.com/NixOS/nixpkgs/blob/68f8b25beaec793642edd731e45594498955acc9/pkgs/development/libraries/v8/8_x.nix#L170-L170
-      #mysql-shell
-      # mysql-workbench # errors out with: No rule to make target '/nix/store/rzinfpv5fmr3iq2rn5mijjwphsx5d9w4-mysql-8.0.31/lib/mysql/libmysqlclient.so', needed by 'libgdal.so.32.3.6.0'.  Stop.
+      usql # universal DB client
     ];
   };
 }
