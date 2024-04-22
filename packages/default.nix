fa5179d3ea078e08dadd9dbf3570c84bef9962c8
Krzysztof Nazarewski <gpg@kdn.im>
2024-04-22 10:20:50 +0200

    feat: drop openapi-python-client-cli
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- packages/default.nix
+++ packages/default.nix
@@ -9,7 +9,6 @@ rec {
   kdn-nix = pkgs.callPackage ./kdn-nix { };
   klg = pkgs.callPackage ./klg { };
   klog-time-tracker = pkgs.callPackage ./klog-time-tracker { };
-  openapi-python-client-cli = pkgs.callPackage ./openapi-python-client-cli { };
   pass-secret-service = pkgs.callPackage ./pass-secret-service { };
   pinentry = pkgs.callPackage ./pinentry { };
   ss-util = pkgs.callPackage ./ss-util { };
--- packages/openapi-python-client-cli/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-pkgs.poetry2nix.mkPoetryApplication {
-  python = pkgs.python311;
-  projectDir = ./.;
-  overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (final: prev: {
-    autoflake = prev.autoflake.overridePythonAttrs (old: { buildInputs = (old.buildInputs or [ ]) ++ (with final; [ hatchling ]); });
-    openapi-python-client = prev.openapi-python-client.overridePythonAttrs (old: { buildInputs = (old.buildInputs or [ ]) ++ (with final; [ poetry-core ]); });
-  });
-}
