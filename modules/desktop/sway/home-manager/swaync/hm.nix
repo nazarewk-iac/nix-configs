fd0677c94a36ca4de435ed1a3af625420e8918f6
Krzysztof Nazarewski <gpg@kdn.im>
2024-06-25 11:11:21 +0200

    feat: swaync: add Super+N panel toggle
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/desktop/sway/home-manager/swaync/hm.nix
+++ modules/desktop/sway/home-manager/swaync/hm.nix
@@ -2,10 +2,15 @@
 let
   # already implemented in home-manager
   cfg = config.services.swaync;
+  keys = config.kdn.desktop.sway.keys;
 in
 {
   config = lib.mkIf cfg.enable {
     xdg.dataFile."dbus-1/services/org.erikreider.swaync.service".source =
       "${pkgs.dunst}/share/dbus-1/services/org.erikreider.swaync.service";
+
+    wayland.windowManager.sway.config.keybindings = with keys; {
+      "${super}+N" = "exec ${lib.getExe' cfg.package "swaync-client"} -t -sw";
+    };
   };
 }
