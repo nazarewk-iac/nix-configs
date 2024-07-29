3a271b910cda072cbddc764d6a209ae73c4a68ef
Krzysztof Nazarewski <gpg@kdn.im>
2024-07-29 19:16:50 +0200

    feat: age-plugin-yubikey: add 3 security levels
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/hardware/yubikey/default.nix
+++ modules/hardware/yubikey/default.nix
@@ -12,42 +12,11 @@ in
     devices = lib.mkOption { };
   };
 
+  imports = [
+    ./yubikeys.nix
+  ];
+
   config = lib.mkIf cfg.enable (lib.mkMerge [
-    {
-      kdn.hardware.yubikey.devices.laptop = {
-        enabled = true;
-        serial = "16174038";
-        notes = [ "data in KeePass" ];
-      };
-      # age-plugin-yubikey Slot 1 is PIV Slot 82
-      kdn.hardware.yubikey.devices.laptop.piv."82" = {
-        type = "age-plugin-yubikey";
-        age-plugin-yubikey = {
-          name = "age identity 97b9264f";
-          pin-policy = "always";
-          touch-policy = "cached";
-          notes = [ "sops" ];
-          recipient = "age1yubikey1q05un9a8q2x783srmhv4hm3pjsrxvgrw92q70yyzjmlx3wnd8jwn2x8ghlp";
-          identity = "AGE-PLUGIN-YUBIKEY-16M9LVQYZJ7UJVNC8Z7TW7";
-        };
-      };
-      kdn.hardware.yubikey.devices.desktop = {
-        enabled = true;
-        serial = "1617439";
-        notes = [ "data in KeePass" ];
-      };
-      kdn.hardware.yubikey.devices.desktop.piv."82" = {
-        type = "age-plugin-yubikey";
-        age-plugin-yubikey = {
-          notes = [ "sops" ];
-          name = "age identity cd6e23c9";
-          pin-policy = "always";
-          touch-policy = "cached";
-          recipient = "age1yubikey1qdppx4kd82ecfxr5lcmgef9w4zxmreyl2q3xv6dsq2jwgv274cm5zmjuy34";
-          identity = "AGE-PLUGIN-YUBIKEY-16L9LVQYZE4HZ8JGY5HK5P";
-        };
-      };
-    }
     {
       # General YubiKey configs
       services.udev.packages = with pkgs;[
@@ -56,11 +25,12 @@ in
       environment.systemPackages = with pkgs; [
         xkcdpass
         yubikey-manager
-        #yubikey-manager-qt # TODO: 2023-03-03 failed to build with ERROR: Could not find a version that satisfies the requirement cryptography<39,>=2.1
         yubikey-personalization
-        yubikey-personalization-gui
         yubico-pam
-      ];
+      ] ++ lib.optionals config.kdn.headless.enableGUI (with pkgs; [
+        #yubikey-manager-qt # TODO: 2023-03-03 failed to build with ERROR: Could not find a version that satisfies the requirement cryptography<39,>=2.1
+        yubikey-personalization-gui
+      ]);
     }
     {
       # GNUPG configs
@@ -106,7 +76,7 @@ in
       # SOPS+age config
       services.pcscd.enable = true;
       environment.systemPackages = with pkgs; [
-        (pkgs.callPackage ./sops.nix { })
+        (pkgs.callPackage ./sops { })
         age
         rage
         age-plugin-yubikey
@@ -116,13 +86,25 @@ in
         xdg.configFile."sops/age/keys.txt".text = lib.pipe cfg.devices [
           (lib.attrsets.mapAttrsToList (_: yk: lib.attrsets.mapAttrsToList
             (slotNum: slot:
-              lib.optional (slot.type == "age-plugin-yubikey") (with slot."${slot.type}"; ''
-                #      Yubikey: ${yk.serial}
-                #     PIV Slot: ${slotNum}
-                #  Plugin Slot: ${builtins.toString (lib.strings.toInt slotNum - 82)}
-                #    Recipient: ${recipient}
-                ${identity}
-              ''))
+              lib.optional (slot.type == "age-plugin-yubikey") (
+                let p = slot."${slot.type}"; in ''
+                  #         Type: age-plugin-yubikey
+                  #      Yubikey: ${yk.serial}
+                  #     PIV Slot: ${slotNum}
+                  #  Plugin Slot: ${builtins.toString (lib.strings.toInt slotNum - 82 + 1)}
+                  #   PIN Policy: ${p."pin-policy"}
+                  # Touch Policy: ${p."touch-policy"}
+                  #    Recipient: ${p.recipient}
+                  # Notes:
+                  ${lib.pipe p.notes [
+                    (builtins.map (lib.strings.splitString "\n"))
+                    lib.flatten
+                    (builtins.map (n: "#   ${n}"))
+                    (builtins.concatStringsSep "\n")
+                  ]}
+                  ${p.identity}
+                ''
+              ))
             yk.piv))
           lib.flatten
           (builtins.concatStringsSep "\n")
---
+++ modules/hardware/yubikey/yubikeys.nix
@@ -0,0 +1,79 @@
+{ lib, config, ... }: {
+  kdn.hardware.yubikey.devices.laptop = {
+    enabled = true;
+    serial = "16174038";
+    notes = [ "data in KeePass" ];
+  };
+  # age-plugin-yubikey Slot 1 is PIV Slot 82
+  kdn.hardware.yubikey.devices.laptop.piv."82" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity 97b9264f";
+      pin-policy = "always";
+      touch-policy = "cached";
+      recipient = "age1yubikey1q05un9a8q2x783srmhv4hm3pjsrxvgrw92q70yyzjmlx3wnd8jwn2x8ghlp";
+      identity = "AGE-PLUGIN-YUBIKEY-16M9LVQYZJ7UJVNC8Z7TW7";
+    };
+  };
+  kdn.hardware.yubikey.devices.laptop.piv."83" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity 6f7792a6";
+      pin-policy = "never";
+      touch-policy = "cached";
+      recipient = "age1yubikey1qfx38en49qfpyjf5hwn8dch6c0jvvtus29pvwgqlze35rraea00y6dw6rwv";
+      identity = "AGE-PLUGIN-YUBIKEY-16M9LVQYRDAME9FSZZVPJA";
+    };
+  };
+  kdn.hardware.yubikey.devices.laptop.piv."84" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity aba74437";
+      pin-policy = "never";
+      touch-policy = "never";
+      recipient = "age1yubikey1q25g9xpunct74wnn29dgcgep78q666dkhpfg2flltg92fvyrew02j28464d";
+      identity = "AGE-PLUGIN-YUBIKEY-16M9LVQYY4WN5GDCQNHWQ0";
+    };
+  };
+  kdn.hardware.yubikey.devices.desktop = {
+    enabled = true;
+    serial = "1617439";
+    notes = [ "data in KeePass" ];
+  };
+  kdn.hardware.yubikey.devices.desktop.piv."82" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity cd6e23c9";
+      pin-policy = "always";
+      touch-policy = "cached";
+      recipient = "age1yubikey1qdppx4kd82ecfxr5lcmgef9w4zxmreyl2q3xv6dsq2jwgv274cm5zmjuy34";
+      identity = "AGE-PLUGIN-YUBIKEY-16L9LVQYZE4HZ8JGY5HK5P";
+    };
+  };
+  kdn.hardware.yubikey.devices.desktop.piv."83" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity a77792a9";
+      pin-policy = "never";
+      touch-policy = "cached";
+      recipient = "age1yubikey1qf4cjj3ksn60z9hnezkwysvgcll7y3mfxzwzdrzgkfkqe9thmczg6xmmfz9";
+      identity = "AGE-PLUGIN-YUBIKEY-16L9LVQYR5AME92GT4N5C3";
+    };
+  };
+  kdn.hardware.yubikey.devices.desktop.piv."84" = {
+    type = "age-plugin-yubikey";
+    age-plugin-yubikey = {
+      notes = [ "sops" ];
+      name = "age identity 3388da78";
+      pin-policy = "never";
+      touch-policy = "never";
+      recipient = "age1yubikey1q0ke6fjulkqg0x0elweecppfkqqr6cqsr42u8q5k5z6466mu60gjseq6p4t";
+      identity = "AGE-PLUGIN-YUBIKEY-16L9LVQYYXWYD57QEX0MMM";
+    };
+  };
+}
