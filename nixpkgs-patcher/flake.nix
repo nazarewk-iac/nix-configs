87782738e0093abde2d4ab719bf93ed227d3a39b
Krzysztof Nazarewski <gpg@kdn.im>
2024-11-12 12:32:15 +0100

    fix: nixpkgs-patcher: "proper" patch format
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- nixpkgs-patcher/flake.nix
+++ nixpkgs-patcher/flake.nix
@@ -1,9 +1,9 @@
 {
   inputs.nixpkgs-upstream.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixpkgs.url = "github:nazarewk/nixpkgs/nixos-unstable";
-  inputs.nixpkgs-patch-2.url = "https://github.com/nazarewk/nixpkgs/compare/nixos-unstable..netbird-improvements.patch";
+  inputs.nixpkgs-patch-2.url = "https://github.com/nixos/nixpkgs/compare/nixos-unstable..nazarewk:netbird-improvements.patch";
   inputs.nixpkgs-patch-2.flake = false;
-  inputs.nixpkgs-patch-3.url = "https://github.com/nazarewk/nixpkgs/compare/nixos-unstable..add/idea-ultimate-eap.patch";
+  inputs.nixpkgs-patch-3.url = "https://github.com/nixos/nixpkgs/compare/nixos-unstable..nazarewk:add/idea-ultimate-eap.patch";
   inputs.nixpkgs-patch-3.flake = false;
 
   outputs = _: { };
