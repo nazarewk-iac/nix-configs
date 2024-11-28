488514486221a21422cf2e62c2c4ee922b248d62
Krzysztof Nazarewski <gpg@kdn.im>
2024-11-28 15:46:50 +0100

    fix: kdn-anonymize: linter fail on shebang
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- packages/kdn-anonymize/default.nix
+++ packages/kdn-anonymize/default.nix
@@ -1,1 +1,13 @@
-{pkgs}: pkgs.writers.writePython3Bin "kdn-anonymize" {} (builtins.readFile ./kdn-anonymize.py)
+{
+  pkgs,
+  lib,
+}: let
+  source = lib.pipe ./kdn-anonymize.py [
+    builtins.readFile
+    (lib.strings.splitString "\n")
+    # drop shebang so the validator doesn't complain about: E265 block comment should start with '# '
+    builtins.tail
+    (builtins.concatStringsSep "\n")
+  ];
+in
+  pkgs.writers.writePython3Bin "kdn-anonymize" {} source
