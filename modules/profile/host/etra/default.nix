128622cd3a42bf967c1867c4ad0fb062217964e2
Krzysztof Nazarewski <gpg@kdn.im>
2025-01-02 12:01:12 +0100

    feat: etra: replace eMMC with NVME drive
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/profile/host/etra/default.nix
+++ modules/profile/host/etra/default.nix
@@ -75,22 +75,22 @@ in {
         "sdhci_pci"
       ];
     }
-    (
-      let
-        cfg = config.kdn.hardware.disks;
-        name = "emmc-etra";
-        luksVol = cfg.luks.volumes."${name}";
-      in {
-        kdn.hardware.disks.initrd.failureTarget = "rescue.target";
-        kdn.hardware.disks.enable = true;
-        kdn.hardware.disks.devices."boot".path = "/dev/disk/by-id/usb-Lexar_USB_Flash_Drive_04R5Q5DX7R12U7QB-0:0";
-        kdn.hardware.disks.luks.volumes."${name}" = {
-          targetSpec.path = "/dev/disk/by-id/mmc-SCA128_0x061748d6";
-          uuid = "696c5033-c9e8-4ce5-be8c-c9fe17566d2e";
-          headerSpec.num = 2;
-        };
-      }
-    )
+    {
+      kdn.hardware.disks.initrd.failureTarget = "rescue.target";
+      kdn.hardware.disks.enable = true;
+      kdn.hardware.disks.devices."boot".path = "/dev/disk/by-id/usb-Lexar_USB_Flash_Drive_04R5Q5DX7R12U7QB-0:0";
+
+      #kdn.hardware.disks.luks.volumes."emmc-etra" = {
+      #  targetSpec.path = "/dev/disk/by-id/mmc-SCA128_0x061748d6";
+      #  uuid = "696c5033-c9e8-4ce5-be8c-c9fe17566d2e";
+      #  headerSpec.num = 2;
+      #};
+      kdn.hardware.disks.luks.volumes."980pro-etra" = {
+        targetSpec.path = "/dev/disk/by-id/nvme-eui.002538b841a03e40";
+        uuid = "9fbfa860-9833-4bde-8cb1-1e80e1e59a65";
+        headerSpec.num = 3;
+      };
+    }
     {
       kdn.networking.router.enable = true;
       # seems to spam logs too much
