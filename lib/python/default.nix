9b4e798fb4317583fcb39caa2c3f6f8ff2ec7085
Krzysztof Nazarewski <gpg@kdn.im>
2025-01-29 18:06:48 +0100

    feat: init: mkPythonScript
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

---
+++ lib/python/default.nix
@@ -0,0 +1,7 @@
+{lib, ...}: let
+  mkPythonScript = pkgs: import ./mkPythonScript.nix {inherit lib pkgs;};
+in {
+  inherit
+    mkPythonScript
+    ;
+}
---
+++ lib/python/mkPythonScript.nix
@@ -0,0 +1,98 @@
+{
+  pkgs,
+  lib,
+}: {
+  src, # ./.
+  name, # some-thing
+  python, # pkgs.python313
+  # defaults
+  scriptFile ? (src + /${name}.py),
+  requirementsFile ? (src + /requirements.txt),
+  devRequirementsFile ? (src + /requirements.dev.txt),
+  containerTag ? "latest",
+  devPackages ? (pp: with pp; [pip-chill]),
+  packageOverrides ? (final: prev: {
+    #  htpy = lib.customisation.callPackageWith (pkgs // prev) ./htpy {};
+    #  nuitka = (lib.customisation.callPackageWith (pkgs // prev) ./nuitka {}).overrideAttrs (old: {
+    #    # do not run checks during development, they seem to take forever
+    #    doCheck = false;
+    #    doInstallCheck = false;
+    #  });
+  }),
+  runtimeDeps ? [],
+  makeWrapperArgs ? [],
+  ...
+}: let
+  readRequirementNames = path:
+    lib.trivial.pipe path [
+      (p:
+        if builtins.pathExists p
+        then builtins.readFile p
+        else "")
+      (lib.strings.splitString "\n")
+      (builtins.map (builtins.match "^([[:alnum:]_-]+).*$"))
+      lib.lists.flatten
+      (builtins.filter (line: line != "" && line != null))
+    ];
+
+  mkPythonDeps = path: pp: builtins.map (pkgName: pp."${pkgName}") (readRequirementNames path);
+
+  pythonInstance = python.override {inherit packageOverrides;};
+
+  mkEnv = depsFn:
+    lib.pipe pythonInstance [
+      (p:
+        p.buildEnv.override {
+          extraLibs = depsFn p.pkgs;
+        })
+      (env:
+        if runtimeDeps == []
+        then env
+        else
+          pkgs.runCommand "${env.name}-with-runtime-deps" {
+            meta.mainProgram = env.executable;
+            unwrapped = env;
+            nativeBuildInputs = with pkgs; [
+              makeBinaryWrapper
+            ];
+          } ''
+            mkdir -p "$out/bin"
+            for binary in "$unwrapped"/bin/* ; do
+              if test -d "$binary" || ! test -x "$binary" ; then
+                continue
+              fi
+              binName="''${binary##*/}"
+              makeBinaryWrapper "$binary" "$out/bin/$binName" \
+                 --inherit-argv0 \
+                 ${lib.strings.escapeShellArgs makeWrapperArgs} \
+                 --prefix PATH : '${lib.makeBinPath runtimeDeps}'
+            done
+          '')
+    ];
+
+  releaseEnv = mkEnv (mkPythonDeps requirementsFile);
+  devEnv = mkEnv (pp:
+    (mkPythonDeps requirementsFile pp)
+    ++ (mkPythonDeps devRequirementsFile pp)
+    ++ (devPackages pp));
+
+  extraOutputs = {
+    python = pythonInstance;
+    releaseEnv = releaseEnv;
+    devEnv = devEnv;
+
+    container = pkgs.dockerTools.buildImage {
+      name = name;
+      tag = "latest";
+      config.Entrypoint = [(lib.getExe script)];
+    };
+  };
+  pythonWriter = pkgs.writers.makeScriptWriter {
+    interpreter = lib.getExe releaseEnv;
+  };
+  script = lib.trivial.pipe scriptFile [
+    (pythonWriter "/bin/${name}")
+    (pkg: pkg // extraOutputs)
+  ];
+in
+  script
