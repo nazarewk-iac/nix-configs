8099e30a8e4ea46799074f4a0dcc9b5686b65c72
Krzysztof Nazarewski <gpg@kdn.im>
2025-04-12 10:25:26 +0200

    feat: persist video editing configs
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/nixos/programs/editors/video/default.nix
+++ modules/nixos/programs/editors/video/default.nix
@@ -9,10 +9,48 @@ in {
   options.kdn.programs.editors.video.enable = lib.mkEnableOption "Video editing software";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      shotcut
-      kdePackages.kdenlive
-      handbrake
+    home-manager.sharedModules = [
+      {
+        kdn.programs.apps.kdenlive = {
+          enable = true;
+          package.original = pkgs.kdePackages.kdenlive;
+          dirs.cache = [];
+          dirs.config = ["kdenlive"];
+          dirs.data = [
+            "kdenlive"
+            "kxmlgui5/kdenlive"
+          ];
+          dirs.disposable = [];
+          dirs.reproducible = [];
+          dirs.state = [];
+          files.config = [
+            "kdenliverc"
+            "kdenlive-layoutsrc"
+          ];
+          files.data = [
+            "knewstuff3"
+          ];
+        };
+        kdn.programs.apps.shotcut = {
+          enable = true;
+          dirs.cache = [];
+          dirs.config = ["Meltytech/Shotcut"];
+          dirs.data = ["Meltytech/Shotcut"];
+          dirs.disposable = [];
+          dirs.reproducible = [];
+          dirs.state = [];
+          files.config = ["Meltytech/Shotcut.conf"];
+        };
+        kdn.programs.apps.handbrake = {
+          enable = true;
+          dirs.cache = [];
+          dirs.config = ["HandBrake" "ghb"];
+          dirs.data = [];
+          dirs.disposable = [];
+          dirs.reproducible = [];
+          dirs.state = [];
+        };
+      }
     ];
   };
 }
