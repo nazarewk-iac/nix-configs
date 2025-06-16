e9dbf5246f03d0103f37134e3b1dadce05dd41d6
Krzysztof Nazarewski <gpg@kdn.im>
2025-06-16 12:01:09 +0200

    feat: slack: group as a separate audio application
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/nixos/programs/slack/hm.nix
+++ modules/nixos/programs/slack/hm.nix
@@ -12,6 +12,34 @@ in {
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
+      xdg.configFile."pipewire/pipewire.conf.d/51-slack.conf".text = builtins.toJSON {
+        /*
+        there is no rule matching on client objects
+        in pulseaudio, you can use pulse.rules with update-props, that should update the client properties
+
+        see https://matrix.to/#/!kySKEYzDwVhedDCSoX:matrix.org/$68PPYfLJ9ZYxcCUR43Kn6I8uD0XFHvyV6r6eJYdN7-k?via=matrix.org&via=kde.org&via=fedora.im
+        */
+        # PulseAudio Volume Control still sees it as Chromium due to PipeWire Client configuration
+        # but PipeWire Client configuration cannot be edited at all
+        "node.rules" = [
+          {
+            matches = [
+              {
+                "application.process.binary" = config.kdn.programs.apps.slack.package.final.meta.mainProgram;
+              }
+            ];
+            actions = {
+              "update-props" = {
+                "application.icon_name" = "slack";
+                "application.name" = "Slack";
+                "media.name" = "Slack Audio";
+                "node.description" = "Slack";
+                "node.name" = "Slack";
+              };
+            };
+          }
+        ];
+      };
       kdn.programs.apps.slack = {
         enable = true;
         dirs.cache = [];
