b0aabebfc2f3f42e5eeb763b8b2091ead978c2e6
Krzysztof Nazarewski <gpg@kdn.im>
2025-07-07 20:18:57 +0200

    update
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/nixos/toolset/ide/hm.nix
+++ modules/nixos/toolset/ide/hm.nix
@@ -12,7 +12,10 @@ in {
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
-      programs.helix.enable = true;
+      /*
+      TODO: 2025-06-26: didn't build
+      */
+      programs.helix.enable = false;
       programs.helix.settings.theme = "darcula-solid";
       stylix.targets.helix.enable = false;
     }
