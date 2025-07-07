1b87584b0da4eb472db549884ecc20f0774f9e81
Krzysztof Nazarewski <gpg@kdn.im>
2025-07-07 20:20:38 +0200

    chore: moss: add IPv6 management note
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- modules/nixos/profile/host/moss/default.nix
+++ modules/nixos/profile/host/moss/default.nix
@@ -12,6 +12,15 @@ in {
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
+      /*
+      # TODO: change to random IPv6, add as AAAA to CloudFlare
+
+      ipython:
+        import ipaddress, random
+        def random_subnet(net, prefixlen): return ipaddress.ip_network(f"{net.network_address + (random.getrandbits(prefixlen - net.prefixlen) << (128 - prefixlen))}/{prefixlen}")
+
+        random_subnet(ipaddress.ip_network('fd31:e17c:1234::/48'), 56)
+      */
       kdn.profile.machine.hetzner.enable = true;
       kdn.profile.machine.hetzner.ipv6Address = "2a01:4f8:1c0c:56e4::1/64";
       security.sudo.wheelNeedsPassword = false;
