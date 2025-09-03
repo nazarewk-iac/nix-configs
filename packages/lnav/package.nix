b73e593149e49920032f555bffaa38cb2da8eb54
Krzysztof Nazarewski <gpg@kdn.im>
2025-09-03 13:50:47 +0200

    feat: lnav: update
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- packages/lnav/package.nix
+++ packages/lnav/package.nix
@@ -21,90 +21,94 @@
   rustc,
   libunistring,
   prqlSupport ? stdenv.hostPlatform == stdenv.buildPlatform,
-}:
-stdenv.mkDerivation (finalAttrs: {
-  pname = "lnav";
-  version = "0.13.0-beta2";
+}: let
+  version = "0.13.1-rc1";
+  srcHash = "sha256-hV7Wd/KyEumHpEFBZnou8TC0+olbAD4dWP6kEXuZSHA=";
+  cargoHash = "sha256-hXjn2CF4FxCfDzikWif9hGWRmlIJI+nxbcV8EBEWxis=";
+in
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "lnav";
+    version = version;
 
-  src = fetchFromGitHub {
-    owner = "tstack";
-    repo = "lnav";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-GAkMQfbyqKUOJU+apFg3Wn7nEVVbP76E+scIHyEr/q8=";
-  };
+    src = fetchFromGitHub {
+      owner = "tstack";
+      repo = "lnav";
+      tag = "v${finalAttrs.version}";
+      hash = srcHash;
+    };
 
-  patches = [
-  ];
-
-  enableParallelBuilding = true;
-
-  separateDebugInfo = true;
-
-  strictDeps = true;
-
-  depsBuildBuild = [buildPackages.stdenv.cc];
-
-  nativeBuildInputs =
-    [
-      autoconf
-      automake
-      zlib
-      curl.dev
-      re2c
-    ]
-    ++ lib.optionals prqlSupport [
-      cargo
-      rustPlatform.cargoSetupHook
-      rustc
+    patches = [
     ];
 
-  buildInputs =
-    [
-      bzip2
-      pcre2
-      readline
-      sqlite
-      curl
-      libarchive
-      libunistring
-    ]
-    ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
-      gpm
-    ];
+    enableParallelBuilding = true;
 
-  cargoDeps = rustPlatform.fetchCargoVendor {
-    src = "${finalAttrs.src}/src/third-party/prqlc-c";
-    hash = "sha256-svi+C3ELw6Ly0mtji8xOv+DDqR0z5shFNazHa3kDQVg=";
-  };
+    separateDebugInfo = true;
 
-  cargoRoot = "src/third-party/prqlc-c";
+    strictDeps = true;
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
+    depsBuildBuild = [buildPackages.stdenv.cc];
 
-  passthru.updateScript = nix-update-script {};
+    nativeBuildInputs =
+      [
+        autoconf
+        automake
+        zlib
+        curl.dev
+        re2c
+      ]
+      ++ lib.optionals prqlSupport [
+        cargo
+        rustPlatform.cargoSetupHook
+        rustc
+      ];
 
-  meta = {
-    homepage = "https://github.com/tstack/lnav";
-    description = "Logfile Navigator";
-    longDescription = ''
-      The log file navigator, lnav, is an enhanced log file viewer that takes
-      advantage of any semantic information that can be gleaned from the files
-      being viewed, such as timestamps and log levels. Using this extra
-      semantic information, lnav can do things like interleaving messages from
-      different files, generate histograms of messages over time, and providing
-      hotkeys for navigating through the file. It is hoped that these features
-      will allow the user to quickly and efficiently zero in on problems.
+    buildInputs =
+      [
+        bzip2
+        pcre2
+        readline
+        sqlite
+        curl
+        libarchive
+        libunistring
+      ]
+      ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+        gpm
+      ];
+
+    cargoDeps = rustPlatform.fetchCargoVendor {
+      src = "${finalAttrs.src}/src/third-party/prqlc-c";
+      hash = cargoHash;
+    };
+
+    cargoRoot = "src/third-party/prqlc-c";
+
+    preConfigure = ''
+      ./autogen.sh
     '';
-    downloadPage = "https://github.com/tstack/lnav/releases";
-    license = lib.licenses.bsd2;
-    maintainers = with lib.maintainers; [
-      dochang
-      symphorien
-      pcasaretto
-    ];
-    platforms = lib.platforms.unix;
-    mainProgram = "lnav";
-  };
-})
+
+    passthru.updateScript = nix-update-script {};
+
+    meta = {
+      homepage = "https://github.com/tstack/lnav";
+      description = "Logfile Navigator";
+      longDescription = ''
+        The log file navigator, lnav, is an enhanced log file viewer that takes
+        advantage of any semantic information that can be gleaned from the files
+        being viewed, such as timestamps and log levels. Using this extra
+        semantic information, lnav can do things like interleaving messages from
+        different files, generate histograms of messages over time, and providing
+        hotkeys for navigating through the file. It is hoped that these features
+        will allow the user to quickly and efficiently zero in on problems.
+      '';
+      downloadPage = "https://github.com/tstack/lnav/releases";
+      license = lib.licenses.bsd2;
+      maintainers = with lib.maintainers; [
+        dochang
+        symphorien
+        pcasaretto
+      ];
+      platforms = lib.platforms.unix;
+      mainProgram = "lnav";
+    };
+  })
