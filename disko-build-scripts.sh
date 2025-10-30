a2a7b64a49edc7602c3d0466df2603769469d04e
Krzysztof Nazarewski <gpg@kdn.im>
2025-10-30 13:48:37 +0100

    misc: missing whitespace
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- disko-build-scripts.sh
+++ disko-build-scripts.sh
@@ -3,7 +3,7 @@ set -eEuo pipefail
 trap 'echo "Error when executing $BASH_COMMAND at line $LINENO!" >&2' ERR
 cd "${BASH_SOURCE[0]%/*}"
 # shellcheck disable=SC2059
-info() { printf "[$(date -Iseconds)]${1}\n" "${@:2}" >&2; }
+info() { printf "[$(date -Iseconds)] ${1}\n" "${@:2}" >&2; }
 info STARTING
 trap 'info FINISHED' EXIT
 test -z "${DEBUG:-}" || set -x
