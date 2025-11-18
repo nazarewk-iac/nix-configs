8573a5fc586a2a216cce37be2bf7e45caa1bdcf3
Krzysztof Nazarewski <gpg@kdn.im>
2025-11-18 16:04:10 +0100

    feat: git-utils: passthru specific scripts
    Signed-off-by: Krzysztof Nazarewski <gpg@kdn.im>

--- packages/git-utils/default.nix
+++ packages/git-utils/default.nix
@@ -43,82 +43,84 @@
     };
   in
     drv // {bin = "${drv}/bin/${name}";};
+
+  utils = with utils; {
+    g-dir = writeShellScript ./bin/g-dir.sh {
+      prefix = ''
+        GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
+      '';
+    };
+    g-dir-azure-devops = writeShellScript ./bin/g-dir-dev.azure.com.sh {
+      prefix = ''
+        GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
+      '';
+    };
+    g-dir-codecommit = writeShellScript ./bin/g-dir-codecommit.sh {
+      prefix = ''
+        GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
+      '';
+    };
+    g-wt-dir = writeShellScript ./bin/g-wt-dir.sh {
+      prefix = ''
+        GIT_UTILS_KDN_WORKTREES_DIR="''${GIT_UTILS_KDN_WORKTREES_DIR:-"${worktreesDir}"}"
+      '';
+      runtimeInputs = [g-dir];
+    };
+    g-wt-get = writeShellScript ./bin/g-wt-get.sh {
+      runtimeInputs = [
+        g-dir
+        g-wt-dir
+      ];
+    };
+    g-wt-rm = writeShellScript ./bin/g-wt-rm.sh {
+      runtimeInputs = [
+        g-dir
+        g-wt-dir
+      ];
+    };
+    g-get = writeShellScript ./bin/g-get.sh {
+      runtimeInputs = [
+        git
+        g-dir
+        g-remote
+      ];
+    };
+    g-remote = writeShellScript ./bin/g-remote.sh {
+      runtimeInputs = [];
+    };
+    g-open = writeShellScript ./bin/g-open.sh {
+      prefix = ''
+        GIT_UTILS_KDN_IDE="''${GIT_UTILS_KDN_IDE:-"${ide}"}"
+      '';
+      runtimeInputs = [
+        git
+        g-dir
+      ];
+    };
+    gh-repos = writeShellScript ./bin/gh-repos.sh {
+      runtimeInputs = [
+        gh
+        jq
+      ];
+    };
+    gh-get-all = writeShellScript ./bin/gh-get-all.sh {
+      runtimeInputs = [gh-repos];
+    };
+
+    gl-repos = writeShellScript ./bin/gl-repos.sh {
+      runtimeInputs = [
+        git
+        curl
+        jq
+      ];
+    };
+    gl-get-all = writeShellScript ./bin/gl-get-all.sh {
+      runtimeInputs = [gl-repos];
+    };
+  };
 in
   symlinkJoin {
     name = "git-utils";
-    passthru = {inherit baseDir ide worktreesDir;};
-    paths = lib.attrsets.attrValues rec {
-      g-dir = writeShellScript ./bin/g-dir.sh {
-        prefix = ''
-          GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
-        '';
-      };
-      g-dir-azure-devops = writeShellScript ./bin/g-dir-dev.azure.com.sh {
-        prefix = ''
-          GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
-        '';
-      };
-      g-dir-codecommit = writeShellScript ./bin/g-dir-codecommit.sh {
-        prefix = ''
-          GIT_UTILS_KDN_BASE_DIR="''${GIT_UTILS_KDN_BASE_DIR:-"${baseDir}"}"
-        '';
-      };
-      g-wt-dir = writeShellScript ./bin/g-wt-dir.sh {
-        prefix = ''
-          GIT_UTILS_KDN_WORKTREES_DIR="''${GIT_UTILS_KDN_WORKTREES_DIR:-"${worktreesDir}"}"
-        '';
-        runtimeInputs = [g-dir];
-      };
-      g-wt-get = writeShellScript ./bin/g-wt-get.sh {
-        runtimeInputs = [
-          g-dir
-          g-wt-dir
-        ];
-      };
-      g-wt-rm = writeShellScript ./bin/g-wt-rm.sh {
-        runtimeInputs = [
-          g-dir
-          g-wt-dir
-        ];
-      };
-      g-get = writeShellScript ./bin/g-get.sh {
-        runtimeInputs = [
-          git
-          g-dir
-          g-remote
-        ];
-      };
-      g-remote = writeShellScript ./bin/g-remote.sh {
-        runtimeInputs = [];
-      };
-      g-open = writeShellScript ./bin/g-open.sh {
-        prefix = ''
-          GIT_UTILS_KDN_IDE="''${GIT_UTILS_KDN_IDE:-"${ide}"}"
-        '';
-        runtimeInputs = [
-          git
-          g-dir
-        ];
-      };
-      gh-repos = writeShellScript ./bin/gh-repos.sh {
-        runtimeInputs = [
-          gh
-          jq
-        ];
-      };
-      gh-get-all = writeShellScript ./bin/gh-get-all.sh {
-        runtimeInputs = [gh-repos];
-      };
-
-      gl-repos = writeShellScript ./bin/gl-repos.sh {
-        runtimeInputs = [
-          git
-          curl
-          jq
-        ];
-      };
-      gl-get-all = writeShellScript ./bin/gl-get-all.sh {
-        runtimeInputs = [gl-repos];
-      };
-    };
+    passthru = {inherit baseDir ide worktreesDir;} // utils;
+    paths = lib.attrsets.attrValues utils;
   }
