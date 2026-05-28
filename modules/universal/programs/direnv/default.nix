fec47cb79d7b4e3fd4a8b6e7329c4c3b0ffcd501
Krzysztof Nazarewski <kristof.nazarewski@onepeloton.com>
2026-05-28 18:26:27 +0200

    fix(direnv): enable for non-NixOS

--- modules/universal/programs/direnv/default.nix
+++ modules/universal/programs/direnv/default.nix
@@ -14,32 +14,36 @@ in
     enable = lib.mkEnableOption "nix-direnv setup";
   };
 
-  config = kdnConfig.util.ifTypes [ "nixos" ] (
-    lib.mkIf cfg.enable {
-      # nix options for derivations to persist garbage collection in devenv
-      nix.extraOptions = ''
-        keep-outputs = true
-        keep-derivations = true
-      '';
-      ## if you also want support for flakes (this makes nix-direnv use the
-      ## unstable version of nix):
-      #nixpkgs.overlays = [
-      #  (final: prev: { nix-direnv = prev.nix-direnv.override { enableFlakes = true; }; })
-      #];
-
-      home-manager.sharedModules = [
-        {
-          home.packages = with pkgs; [
-            direnv
-            nix-direnv
-          ];
-          programs.direnv.enable = true;
-          programs.direnv.nix-direnv.enable = true;
-          programs.git.ignores = [ (builtins.readFile ./.gitignore) ];
-          kdn.disks.persist."usr/data".directories = [ ".local/share/direnv" ];
-          kdn.disks.persist."usr/config".directories = [ ".config/direnv" ];
-        }
-      ];
-    }
-  );
+  config = lib.mkMerge [
+    (kdnConfig.util.ifHMParent {
+      home-manager.sharedModules = [ { kdn.programs.direnv = cfg; } ];
+    })
+    (kdnConfig.util.ifTypes [ "nixos" ] (
+      lib.mkIf cfg.enable {
+        # nix options for derivations to persist garbage collection in devenv
+        nix.extraOptions = ''
+          keep-outputs = true
+          keep-derivations = true
+        '';
+        ## if you also want support for flakes (this makes nix-direnv use the
+        ## unstable version of nix):
+        #nixpkgs.overlays = [
+        #  (final: prev: { nix-direnv = prev.nix-direnv.override { enableFlakes = true; }; })
+        #];
+      }
+    ))
+    (kdnConfig.util.ifHM (
+      lib.mkIf cfg.enable {
+        home.packages = with pkgs; [
+          direnv
+          nix-direnv
+        ];
+        programs.direnv.enable = true;
+        programs.direnv.nix-direnv.enable = true;
+        programs.git.ignores = [ (builtins.readFile ./.gitignore) ];
+        kdn.disks.persist."usr/data".directories = [ ".local/share/direnv" ];
+        kdn.disks.persist."usr/config".directories = [ ".config/direnv" ];
+      }
+    ))
+  ];
 }
